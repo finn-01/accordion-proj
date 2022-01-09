4fba0314744a70b7075f42aede2147eaf66ca1a0
finn-01 <nguyensam576@gmail.com>
2022-01-09 20:39:26 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Question from "./Question";
 import data from "./data";
 
 const App = () => {
+	const [questions, setQuestions] = useState(data);
+
 	return (
-		<div>
-			Hello App
-			<Question />
+		<div className="container">
+			<h3>questions and answers about login</h3>
+			<section className="info">
+				{questions.map((question) => {
+					return <Question key={question.id} {...question} />;
+				})}
+			</section>
 		</div>
 	);
 };
