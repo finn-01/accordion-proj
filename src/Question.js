4d6bc696816b1f384a2d0c39d503e1de2420f2e1
finn-01 <nguyensam576@gmail.com>
2022-01-09 20:22:03 +0700

    setup

---
+++ src/Question.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = () => {
+	return <div>Hello Queston</div>;
+};
+
+export default Question;
